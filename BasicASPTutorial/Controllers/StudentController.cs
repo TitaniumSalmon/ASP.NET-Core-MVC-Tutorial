61b4ecbb7382238c93bbb55e4b7cdd69f4bbf49f
TitaniumSalmon <perapon.s@ku.th>
2025-07-03 03:24:37 +0700

    Multi Student Info Display

--- BasicASPTutorial/Controllers/StudentController.cs
+++ BasicASPTutorial/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BasicASPTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BasicASPTutorial.Controllers
 {
@@ -6,7 +7,26 @@ namespace BasicASPTutorial.Controllers
     {
         public IActionResult Index()
         {
-            return View(); 
+            Student s1 = new Student();
+            s1.Id = 1;
+            s1.Name = "John Nexon";
+            s1.Score = 100;
+
+            Student s2 = new Student();
+            s2.Id = 2;
+            s2.Name = "John Cena";
+            s2.Score = 49;
+
+            Student s3 = new Student();
+            s3.Id = 3;
+            s3.Name = "Jong Sena";
+            s3.Score = 50;
+
+            List<Student> students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+            students.Add(s3);
+            return View(students); 
         }
         public IActionResult Create()
         {
--- BasicASPTutorial/Models/Student.cs
+++ BasicASPTutorial/Models/Student.cs
@@ -2,5 +2,8 @@
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
     }
 }
