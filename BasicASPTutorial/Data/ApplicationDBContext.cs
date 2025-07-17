762fcf91287091d219f8a68cea6ce620d693493b
TitaniumSalmon <perapon.s@ku.th>
2025-07-17 16:05:43 +0700

    Application DBContext

---
+++ BasicASPTutorial/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BasicASPTutorial.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<BasicASPTutorial.Models.Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BasicASPTutorial.Models.Student>().ToTable("Students");
+        }
+}
--- BasicASPTutorial/Models/Student.cs
+++ BasicASPTutorial/Models/Student.cs
@@ -1,9 +1,16 @@
-﻿namespace BasicASPTutorial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicASPTutorial.Models
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Score must be between 0 and 100.")]
         public int Score { get; set; }
     }
 }
